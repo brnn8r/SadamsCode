3716af0d7dc6792e86485c348a032f3ad5d7b29d
Stephen Christopher Adams <vengeanz@gmail.com>
2013-06-03 17:31:28 +1200

    Initial Commit of WMDUtilities

---
+++ WMDUtilities/WMDUtilities/ObjectExtensions/ObjectExtender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMDUtilities.ObjectExtensions.Converter;
+using WMDUtilities.ObjectExtensions.Tracer;
+using WMDUtilities.ObjectExtensions.Cloner;
+
+namespace WMDUtilities.ObjectExtensions
+{
+    public static class ObjectExtender
+    {
+        public static IConverter<T> Converter<T>(this object o)
+        {
+            return new Converter<T>(o);
+        }
+
+        public static ITracer Tracer(this object o)
+        {
+            return new DebugTracer(o);
+        }
+
+        public static ICloner<T> Cloner<T>(this object o)
+        {
+            return new SerializingCloner<T>(o);
+        }
+    }
+}
---
+++ WMDUtilities/WMDUtilitiesTest/ObjectExtensions/ClonerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WMDUtilities.ObjectExtensions;
+using WMDUtilitiesTest.TestData;
+
+namespace WMDUtilitiesTest.ObjectExtensions
+{
+    [TestFixture]
+    public class ClonerTest    
+    {
+        [TestCase(82)]
+        public void TestIntegerClone(int i)
+        {
+            int clone = i.Cloner<int>().Clone();
+
+            clone.Tracer().Trace("clone");
+
+            Assert.AreEqual(clone, i);
+            Assert.IsFalse(ReferenceEquals(clone, i));
+        }
+        [TestCase("I'll Be Back!")]
+        public void TestStringClone(string s)
+        {
+            string clone = s.Cloner<string>().Clone();
+
+            clone.Tracer().Trace("clone");
+
+            Assert.AreEqual(clone, s);
+            Assert.IsFalse(ReferenceEquals(clone, s));
+        }
+    }
+}
